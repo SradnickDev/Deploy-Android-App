30d34201c450a55f67420b69c6f05b1459188b78
Oliver <40965190+SradnickDev@users.noreply.github.com>
2020-05-23 19:37:49 +0200

    added files

---
+++ DeployAppToAllDevices.ps1
@@ -0,0 +1,27 @@
+﻿#requires -Version 3.0
+Function Deploy-App
+{
+  $devices = adb.exe devices
+  
+  $file = Get-ChildItem -Path $PSScriptRoot -Filter *.apk
+  $fileName = $file.Name
+
+  For ($i = 1; $i -le $devices.Length-2; $i++) 
+  {
+    Write-Output ('>> {0}' -f $devices[$i])
+    $device = $devices[$i].Substring(0,$devices[$i].Length - 7)  
+    adb.exe -s $device install -r $file
+  }
+}
+
+Function Show-Confirmation($messasge)
+{
+  $confirmation = Read-Host -Prompt ('{0} (y/n)' -f $messasge)
+  if ($confirmation -eq 'y') 
+  {
+    return $true
+  }
+}
+
+Show-Confirmation -messasge ('Push and Install to all connected devices ?')
+Deploy-App
---
+++ SetupDevices.ps1
@@ -0,0 +1,73 @@
+﻿function Setup-Device
+{
+  $retVal = Show-Confirmation -messasge ('Do you want to connect a USB device?')
+  if(!$retVal) 
+  {
+    return
+  }
+
+  #restart server
+  Write-Output -InputObject 'Restart Server'
+  adb.exe kill-server 2>$null | adb.exe start-server 2>$null
+  
+  Write-Output -InputObject 'Server running'
+  Write-Output -InputObject 'retrieving ip adress'
+  
+  #retrieve ip
+  $file = '{0}\devicesIp.txt' -f $PSScriptRoot
+  $device = adb.exe shell ip route 2>$null
+
+  if(!$device)
+  {
+    $confirmation = Read-Host -Prompt 'No device connected.(press any key to exit)'
+    return
+  }
+  
+  $pos = $device[2].IndexOf('src')
+  $ipAdrr = $device[2].Substring($pos+4)
+
+  $pos = $ipAdrr.IndexOf('metric')
+  $ipAdrr = $ipAdrr.Substring(0,$pos-2)
+  $ipAdrr = '{0}:5555' -f $ipAdrr
+  
+  
+  # start tcp
+  $cmdOutput = adb.exe tcpip 5555
+  Write-Output -InputObject $cmdOutput
+  Write-Output -InputObject 'Server Running'
+
+  Add-Content $file -Value $ipAdrr
+  Write-Output -InputObject "$ipAdrr added to File."
+  
+  Setup-Device
+}
+
+function Connect-Devices
+{ 
+  $retVal = Show-Confirmation -messasge ('Connect all Devices via WIFI ?')
+  
+  if(!$retVal) 
+  {
+    return
+  }
+  
+  $file = '{0}\devicesIp.txt' -f $PSScriptRoot
+  foreach($deviceIp in Get-Content -Path $file) 
+  {
+    adb.exe connect $deviceIp
+  }
+}
+
+Function Show-Confirmation($messasge)
+{
+  $confirmation = Read-Host -Prompt ('{0} (y/n)' -f $messasge)
+  if ($confirmation -eq 'y') 
+  {
+    return $true
+  }
+}
+
+
+### Script Entry ###
+Setup-Device
+Connect-Devices

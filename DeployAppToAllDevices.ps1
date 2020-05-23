7437cfbc1eaaae09999988735dc488cead451e83
Oliver <40965190+SradnickDev@users.noreply.github.com>
2020-05-23 23:49:52 +0200

    Update DeployAppToAllDevices.ps1
    removed Read-Host
    added check if for file

--- DeployAppToAllDevices.ps1
+++ DeployAppToAllDevices.ps1
@@ -1,27 +1,25 @@
-﻿#requires -Version 3.0
+#requires -Version 3.0
+
 Function Deploy-App
 {
+  $file = Get-ChildItem -Path $PSScriptRoot -Filter *.apk
+  
+  if(!$file)
+  {
+    Write-Output -InputObject 'No file found.'
+    return
+  }
+  $fileName = $file.Name
+  Write-Output -InputObject ('Deploy App {0}' -f $fileName)
   $devices = adb.exe devices
   
-  $file = Get-ChildItem -Path $PSScriptRoot -Filter *.apk
-  $fileName = $file.Name
 
   For ($i = 1; $i -le $devices.Length-2; $i++) 
   {
-    Write-Output ('>> {0}' -f $devices[$i])
+    Write-Output -InputObject ('>> {0}' -f $devices[$i])
     $device = $devices[$i].Substring(0,$devices[$i].Length - 7)  
     adb.exe -s $device install -r $file
   }
 }
 
-Function Show-Confirmation($messasge)
-{
-  $confirmation = Read-Host -Prompt ('{0} (y/n)' -f $messasge)
-  if ($confirmation -eq 'y') 
-  {
-    return $true
-  }
-}
-
-Show-Confirmation -messasge ('Push and Install to all connected devices ?')
 Deploy-App
